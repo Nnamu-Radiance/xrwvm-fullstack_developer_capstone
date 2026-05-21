1471897ef6ea6119e738c26d11b67ac0d1999929
Nnamu-Radiance <nnamuradiance@gmail.com>
2026-05-21 10:13:00 -0400

    Add About.html, Contact.html, update urls.py and settings

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.views.generic import TemplateView, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('about/', TemplateView.as_view(template_name='About.html')),
+    path('contact/', TemplateView.as_view(template_name='Contact.html')),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
